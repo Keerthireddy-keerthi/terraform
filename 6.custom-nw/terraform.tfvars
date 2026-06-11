352942e5d8c07e4629031bd093e09d5a94bf2593
Keerthireddy <puthalapattukeerthireddy9@gmail.com>
2026-06-11 16:36:08 +0530

    networking

---
+++ 6.custom-nw/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id = "ami-0152204c1a187337c"
+instance_type = "t3.micro"
+name = "keer-instance"
---
+++ day-10-terraform-import/main.tf
@@ -0,0 +1,23 @@
+
+
+resource "aws_instance" "name" {
+    ami           = "ami-0152204c1a187337c"
+    instance_type = "t3.micro"
+    tags = {
+        Name = "ec2"
+    }
+  
+}
+
+resource "aws_s3_bucket" "name" {
+    bucket = "keerthi-s3-bucket-2026-06-12"
+   
+  
+}
+
+resource "aws_s3_bucket_versioning" "name" {
+    bucket = aws_s3_bucket.name.id
+    versioning_configuration {
+        status = "Suspended"
+    }
+}
